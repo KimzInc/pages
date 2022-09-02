2db65bd43bff00d2571f5baaf359a0c92869e5fe
Felix <kmtflx@gmail.com>
2022-09-02 17:39:25 +0300

    changes made: modification of blog/admin.py to have a proper display on admin section

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'published_date','status')
+    list_filter = ('status', 'created', 'published_date', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_date'
+    ordering = ('status','published_date')
